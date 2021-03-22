bad1a22f9f537068bbed789a1912c83e20c1ba6e
vyshnavi0909 <vyshugoud11@gmail.com>
2021-03-22 20:15:13 +0530

    ADD: Calculated the length of 2 points

--- lineComparison.java
+++ lineComparison.java
@@ -1,6 +1,27 @@
+import java.util.*;
+
 public class lineComparison {
 
 	public static void main(String[] args){
 		System.out.println("Welcome to Line Comparison Computation");
+
+		Scanner input1 = new Scanner(System.in);
+		System.out.println("Enter x1 value: ");
+		int x1 = input1.nextInt();
+
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("Enter y1 value: ");
+		int y1 = input2.nextInt();
+
+		Scanner input3 = new Scanner(System.in);
+      System.out.println("Enter x2 value: ");
+      int x2 = input3.nextInt();
+
+      Scanner input4 = new Scanner(System.in);
+      System.out.println("Enter y2 value: ");
+      int y2 = input4.nextInt();
+
+		double length = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+		System.out.println(length);
 	}
 }
