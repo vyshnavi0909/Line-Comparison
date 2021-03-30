17df3dc97441cef6974f8ac8099e8f2b75074b45
vyshnavi0909 <vyshugoud11@gmail.com>
2021-03-30 10:21:59 +0530

    Modified: method names

--- lineComparison.java
+++ lineComparison.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class lineComparison {
 
 	//Creating a constructor for reading inputs from user
-	static int UserInput(){
+	static int userInput(){
 		Scanner input = new Scanner(System.in);
 		int x = input.nextInt();
 		return x;
 	}
 
 	//Creating a constructor for calculating lengths of the lines
-	static double Length(int a, int b, int c, int d){
+	static double length(int a, int b, int c, int d){
 		double lenOfLine = Math.sqrt(Math.pow((c - a),2) + Math.pow((d - b),2));
 		return lenOfLine;
 	}
@@ -19,18 +19,18 @@ public class lineComparison {
 		System.out.println("Welcome to Line Comparison Computation");
 
 		//reading inputs by calling static method UserInput()
-		int x1 = UserInput();
-		int x2 = UserInput();
-		int x3 = UserInput();
-		int x4 = UserInput();
-		int y1 = UserInput();
-		int y2 = UserInput();
-		int y3 = UserInput();
-		int y4 = UserInput();
+		int x1 = userInput();
+		int x2 = userInput();
+		int x3 = userInput();
+		int x4 = userInput();
+		int y1 = userInput();
+		int y2 = userInput();
+		int y3 = userInput();
+		int y4 = userInput();
 
 		//creating objects for finding lengths
-		double lenOfLine1 = Length(x1, y1, x2, y2);
-		double lenOfLine2 = Length(x3, y3, x4, y4);
+		double lenOfLine1 = length(x1, y1, x2, y2);
+		double lenOfLine2 = length(x3, y3, x4, y4);
 
 		System.out.println("length of First Line: " + lenOfLine1 );
 
